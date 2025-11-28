4681a2f90d195434d6e8256cf764fd4973e2541b
unknown <alamgirhossaindrubo@gmail.com>
2025-11-28 22:53:41 +0600

    Update useAxiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -7,15 +7,37 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
-    const {user} = useAuth();
+    const {user,logout} = useAuth();
 
     useEffect(()=>{
         // Add a request interceptor
-        axiosSecure.interceptors.request.use((config) => {
+        const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
             config.headers.Authorization = `Bearer ${user.accessToken}`;
             return config;
+        });
+
+        const resInterceptor = axiosSecure.interceptors.response.use((response)=>{
+            return response;
+        }, (error)=>{
+            console.log(error);
+            const status = error.status;
+            if(status === 401 || status === 403){
+                logout()
+                    .then(()=>{
+                        console.log('Logout user successfully');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+            }
+            return Promise.reject(error);
         })
-    },[user])
+ 
+        return ()=>{
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        }
+    },[user,logout])
     return axiosSecure;
 };
 
